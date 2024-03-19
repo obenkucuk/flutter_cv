f39682628ba252288a1a73f4581eff075c5c45f4
obenkucuk <obenkucuk@gmail.com>
2024-03-19 18:52:01 +0300

    feat: android firebase

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,12 +1,12 @@
-import 'package:cv/config/flavor/flavors.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:package_info_plus/package_info_plus.dart';
-
 import 'app.dart';
 import 'config/flavor/flavor_model.dart';
+import 'config/flavor/flavors.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,8 +15,10 @@ void main() async {
 
   PackageInfo packageInfo = await PackageInfo.fromPlatform();
 
+  final firebase = await Firebase.initializeApp();
+
   print(
-      'App Name: ${packageInfo.appName}, Version: ${packageInfo.version}, Bundle ID: ${packageInfo.packageName}');
+      'App Name: ${packageInfo.appName}, Version: ${packageInfo.version}, Bundle ID: ${packageInfo.packageName}, Firebase: ${firebase.options.apiKey}, Firebase ID: ${firebase.options.projectId}');
 
   const flavorModel = FlavorModel(
     flavor: Flavors.PRODUCTION,
